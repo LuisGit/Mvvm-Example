c23cb2ec248f2019fd9343901f25988d292f3f3d
Luis Alvarado Solano <luis.alvarado@gorillalogic.com>
2021-01-15 15:05:26 -0600

    Adding right navigation button on the navigation header

---
+++ LuisMvvmExample/LuisMvvmExample/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Extensions.swift
+//  LuisMvvmExample
+//
+//  Created by Luis Alvarado on 1/15/21.
+//
+
+import UIKit
+
+extension UIColor {
+    static let primary = UIColor(red: 0/256, green: 39/256, blue: 255/255, alpha: 0.6)
+}
--- LuisMvvmExample/LuisMvvmExample/ViewControllers/EventListViewController.swift
+++ LuisMvvmExample/LuisMvvmExample/ViewControllers/EventListViewController.swift
@@ -18,6 +18,15 @@ class EventListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let plusImage = UIImage(systemName: "plus.circle.fill")
+        let barButtonItem = UIBarButtonItem(image: plusImage, style: .plain, target: self, action: #selector(tappedRightBarButton))
+        barButtonItem.tintColor = .primary
+        navigationItem.rightBarButtonItem = barButtonItem
+        navigationItem.title = "Events"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    @objc private func tappedRightBarButton(){
+        print("taped right bar button")
     }
 }
